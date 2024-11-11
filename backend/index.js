3ac19ca548884813f45d2543b926bfa8bebf9225
Nijeshpatel11 <napatel3083@gmail.com>
2024-11-12 00:11:46 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
-const dbURI = process.env.MONGO_URI || 'your_mongodb_connection_string';
+const dbURI = process.env.MONGO_URI || 'mongodb+srv://napatel3083:nijesh001@cluster0.u6p1t.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.error('Error connecting to MongoDB:', error));
